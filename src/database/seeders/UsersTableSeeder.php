b43a68b9b1a095502178c3df37ca60889af1c609
sakura <sakura200369@gmail.com>
2025-03-23 18:43:10 +0800

    Add UsersTableSeeder from develop to main

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,40 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'id' => '1',
+            'name' => 'テストユーザー１',
+            'email' => 'test1@gmail.com',
+            'password' => Hash::make('password'),
+            'image' => 'storage/image/girl.png',
+            'postal_code' => null,
+            'address' => null,
+            'building' => null,
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'id' => '2',
+            'name' => 'テストユーザー２',
+            'email' => 'test2@gmail.com',
+            'password' => Hash::make('password'),
+            'image' => 'storage/image/boy.png',
+            'postal_code' => null,
+            'address' => null,
+            'building' => null,
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'id' => '3',
+            'name' => 'テストユーザー３',
+            'email' => 'test3@gmail.com',
+            'password' => Hash::make('password'),
+            'image' => 'storage/image/sakura.jpg',
+            'postal_code' => null,
+            'address' => null,
+            'building' => null,
+        ];
+        DB::table('users')->insert($param);
     }
 }
